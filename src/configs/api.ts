cb57e8f71967b65eec98ca862cbc6243b98e899b
Tallyta <tallyta.martins@acct.global>
2024-04-03 21:20:10 -0300

    feat: baseURL

--- src/configs/api.ts
+++ src/configs/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const baseURL = "http://192.168.1.4:3001/students"
+const baseURL = "https://minha-escola-backend-fatec-tg.vercel.app/students"
 
 export const fetchData = (accessToken: string) => {
   return axios.create({
